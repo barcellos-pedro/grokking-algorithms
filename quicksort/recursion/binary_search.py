e8f3ee90ff7f85ccad6706bc2c75ac59c71dc608
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2023-06-29 16:22:03 -0300

    Refactor binary serach (recursion) example

--- quicksort/recursion/binary_search.py
+++ quicksort/recursion/binary_search.py
@@ -1,6 +1,8 @@
 def binary_search(items, target, min=0, max=0):
     if len(items) == 0:
         return None
+    if len(items) == 1:
+        return 0 if items[0] == target else None
 
     if max == 0:
         max = len(items) - 1
@@ -22,3 +24,5 @@ print(binary_search([1, 2, 3, 4, 5, 6], 1))
 print(binary_search([1, 2, 3, 4, 5, 6], 3))
 print(binary_search([1, 2, 3, 4, 5, 6], 6))
 print(binary_search([1, 2, 3, 4, 5, 6], -1))
+print(binary_search([3], 3))
+print(binary_search([], 1))
